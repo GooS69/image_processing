43569d104b8b41c4b0765e8f8448b9f1668bb168
GooS69 <guseff.daniil2011@yandex.ru>
2020-10-16 20:12:53 +0300

    Added: gauss filter solarisation

--- Main.py
+++ Main.py
@@ -112,6 +112,13 @@ def quantization_click():
     gist(gray_pil_img, gist_label)
 
 
+def solarisation_click():
+    global gray_pil_img, img_label, gist_label
+
+    solarisation(gray_pil_img, img_label)
+    gist(gray_pil_img, gist_label)
+
+
 def low_pass_filter_click():
     global gray_pil_img, img_label, gist_label
 
@@ -130,6 +137,13 @@ def median_filter_click():
     global gray_pil_img, img_label, gist_label, filter_size
 
     median_filter(gray_pil_img, img_label, filter_size * 2 + 1)
+    gist(gray_pil_img, gist_label)
+
+
+def gauss_filter_click():
+    global gray_pil_img, img_label, gist_label, filter_size
+
+    gauss_filter(gray_pil_img, img_label, filter_size * 2 + 1)
     gist(gray_pil_img, gist_label)
 
 
@@ -249,6 +263,17 @@ def quantization_change(pil_img, label, quantization):
             res_pix = (int)(pix[0] / q_lenght)
             res_pix *= q_lenght
             pil_img.putpixel((i, j), (res_pix, res_pix, res_pix))
+    img = ImageTk.PhotoImage(pil_img)
+    label.configure(image=img)
+    label.image = img
+
+
+def solarisation(pil_img, label):
+    all_pix = pil_img.load()
+    for i in range(pil_img.size[0]):
+        for j in range(pil_img.size[1]):
+            pix = (int)(all_pix[i, j][0] * all_pix[i, j][0] * (-255 / 16256) + 65025 / 16256 * all_pix[i, j][0])
+            pil_img.putpixel((i, j), (pix, pix, pix))
     img = ImageTk.PhotoImage(pil_img)
     label.configure(image=img)
     label.image = img
@@ -396,6 +421,63 @@ def median_filter(pil_img, label, size):
     label.image = img
 
 
+def gauss_filter(pil_img, label, size):
+    global gray_pil_img
+
+    all_pix = pil_img.load()
+
+    half_size = size // 2
+    new_img = Image.new('RGB', (pil_img.size[0], pil_img.size[1]))
+
+    first = [1]
+    second = [1]
+    for i in range(1, size):
+        second.append(1)
+        for j in range(1, i):
+            second[j] = (int)(first[j] + first[j - 1])
+        first = second.copy()
+
+    matrix = []
+    for i in range(size):
+        matrix.append([])
+        for j in range(size):
+            matrix[i].append(j)
+
+    for i in range(size):
+        for j in range(size):
+            matrix[i][j] = first[i] * first[j]
+
+    for i in range(half_size, pil_img.size[0] - half_size - 1):
+        for j in range(half_size, pil_img.size[1] - half_size - 1):
+            sum = 0
+
+            for k in range(-half_size, half_size + 1):
+                for m in range(-half_size, half_size + 1):
+                    sum += all_pix[i + k, j + m][0] * matrix[k + half_size][m + half_size]
+
+            sum = (int)(sum / (2 ** (size - 1)) ** 2)
+            new_img.putpixel((i, j), (sum, sum, sum))
+
+    for k in range(pil_img.size[0]):
+        for m in range(half_size + 1):
+            pix = all_pix[k, m]
+            new_img.putpixel((k, m), (pix[0], pix[0], pix[0]))
+            pix = all_pix[k, new_img.size[1] - 1 - m]
+            new_img.putpixel((k, new_img.size[1] - 1 - m), (pix[0], pix[0], pix[0]))
+
+    for l in range(new_img.size[1]):
+        for n in range(half_size + 1):
+            pix = all_pix[n, l]
+            new_img.putpixel((n, l), (pix[0], pix[0], pix[0]))
+            pix = all_pix[new_img.size[0] - 1 - n, l]
+            new_img.putpixel((new_img.size[0] - 1 - n, l), (pix[0], pix[0], pix[0]))
+
+    gray_pil_img = new_img
+    img = ImageTk.PhotoImage(gray_pil_img)
+    label.configure(image=img)
+    label.image = img
+
+
 window = Tk()
 window.title("Обработка изображений")
 window.geometry('1920x1080')
@@ -448,6 +530,9 @@ scale_quantization.pack()
 quantization_button = Button(text='quantization', command=quantization_click)
 quantization_button.pack()
 
+solarisation_button = Button(text='solarisation', command=solarisation_click)
+solarisation_button.pack()
+
 low_pass_filter_button = Button(text='low pass filter', command=low_pass_filter_click)
 low_pass_filter_button.pack()
 
@@ -459,5 +544,7 @@ scale_filter_size = Scale(window, orient='horizontal', label='size of filter', f
 scale_filter_size.pack()
 median_filter_button = Button(text='median filter', command=median_filter_click)
 median_filter_button.pack()
+gauss_filter_button = Button(text='gauss filter', command=gauss_filter_click)
+gauss_filter_button.pack()
 
 window.mainloop()
